85a746e4a1a93bb8b952fb68731a89f8d7a3c733
Hawng-coder <25112240@st.vju.ac.vn>
2026-04-02 23:13:03 +0700

    nộp bài tập chương 2,3

---
+++ Chuong_2/ex1.2.py
@@ -0,0 +1,15 @@
+# Cau 1
+seconds = 42 * 60 + 42
+print("So giay la:", seconds)
+
+# Cau 2
+miles = 10 / 1.61
+print("So dam la:", miles)
+
+# Cau 3
+total_minutes = 42 + (42/60)
+total_hours = total_minutes / 60
+pace = total_minutes / miles
+print("Thoi gian trung binh moi dam la:", pace)
+speed = miles / total_hours
+print("Van toc trung binh la:", speed)
